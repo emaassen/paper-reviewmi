b5d13c3b4c4d8164576fd6af36c0c40f2a911952
E. Maassen <E.Maassen@tilburguniversity.edu>
2020-07-26 20:41:57 +0200

    new and updated scripts

--- code/articleindex.R
+++ code/articleindex.R
@@ -2,6 +2,7 @@ install.packages("rplos")
 library("rplos")
 library("rcrossref")
 library("dplyr")
+library("tidyr")
 
 # Search for subject psychology
 # Search in journal PLoSONE
@@ -13,7 +14,8 @@ res <- plossubject(q = "\"psychology\"",
             fl=c('id',
                  'publication_date'),
             fq=list('journal_key:PLoSONE',
-                    'article_type:"Research Article"'),
+                    'article_type:"Research Article"',
+                    "doc_type:full"),
             start = 0,
             limit = 0)
 
@@ -23,12 +25,15 @@ res <- plossubject(q = "\"psychology\"",
                    fl=c('id',
                         'publication_date'),
                    fq=list('journal_key:PLoSONE',
-                           'article_type:"Research Article"'),
+                           'article_type:"Research Article"',
+                           "doc_type:full"),
                    start = 0,
                    limit = r)
 
 # back up this huge set before I make a mistake
 backup <- res
+backup2 <- backup
+res <- backup
 
 # fill blank spaces
 res[res == ""] <- NA
@@ -37,13 +42,14 @@ res[res == ""] <- NA
 res$data$publication_date <- substr(res$data$publication_date, 1, 4)
 
 # filter year - without doctype full we have 5169 left
-# filter year - with doctype full we have 5169
+# filter year - with doctype full we have 5169 left
 res$data <- res$data[which(res$data$publication_date >= 2019),]
 
+# are there duplicates?
+res$data$duplicate # this doesnt work
 
-
-
-
+# find unique dois - still 5169
+length(unique(res$data$id))
 
 
 # Don't  be  surprised  if  queries  you  perform  in  a  scripting  
@@ -55,11 +61,27 @@ res$data <- res$data[which(res$data$publication_date >= 2019),]
 # For which journalto search, e.g., do'journal_key:PLoSONE'. 
 # Seejournalnamekey()for journal abbreviations
 
-# ISSN PlosOne = 1932-6203
-# ISSN PsychScience = 1467-9280 (web), 0956-7976 (print)
+# ISSN PlosOne = 1932-6203 
+# ISSN PsychScience = 1467-9280 (web, I find 166), 0956-7976 (print)
 
-cr_journals(issn = c('1932-6203','1467-9280'), from_pub_date = '2019')
+ps <- cr_journals(issn = c('1467-9280'), 
+            filter = c(from_pub_date = '2019-01-01', 
+                      until_pub_date = '2019-12-31'),  
+            works = T,
+            limit = 1000)
+
+plos <- cr_journals(query = "psychology",
+                    issn = c('1932-6203'), 
+                    filter = c(from_pub_date = '2019-01-01', 
+                               until_pub_date = '2019-12-31',
+                               has_full_text = TRUE),  
+                    works = T,
+                    limit = 1000)
+
+# we can't filter on subject so for Plos the number is way too large. 
 
 
-# Get random doi
+# Get 2 random dois
 cr_r(2)
+
+
