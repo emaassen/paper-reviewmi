841bedbf7c9305224251a4bbff0f51e4e5974122
E. Maassen <E.Maassen@tilburguniversity.edu>
2020-07-28 14:55:51 +0200

    sampling PLOS articles code

--- code/articleindex.R
+++ code/articleindex.R
@@ -1,4 +1,3 @@
-install.packages("rplos")
 library("rplos")
 library("rcrossref")
 library("dplyr")
@@ -9,7 +8,7 @@ library("tidyr")
 # Search for research articles (no corrections, viewpoints etc.)
 # Only show full text availability
 
-
+# Check how many articles there are
 res <- plossubject(q = "\"psychology\"", 
             fl=c('id',
                  'publication_date'),
@@ -19,8 +18,10 @@ res <- plossubject(q = "\"psychology\"",
             start = 0,
             limit = 0)
 
+# Save number of articles
 r <- res$meta$numFound
 
+# Index all articles from 0 to r
 res <- plossubject(q = "\"psychology\"", 
                    fl=c('id',
                         'publication_date'),
@@ -32,56 +33,35 @@ res <- plossubject(q = "\"psychology\"",
 
 # back up this huge set before I make a mistake
 backup <- res
-backup2 <- backup
-res <- backup
-
-# fill blank spaces
-res[res == ""] <- NA
 
 # change publication date to only the year
 res$data$publication_date <- substr(res$data$publication_date, 1, 4)
+res
 
 # filter year - without doctype full we have 5169 left
 # filter year - with doctype full we have 5169 left
-res$data <- res$data[which(res$data$publication_date >= 2019),]
+index2019 <- res$data[which(res$data$publication_date == 2019),]
+index2018 <- res$data[which(res$data$publication_date == 2018),]
 
-# are there duplicates?
-res$data$duplicate # this doesnt work
-
-# find unique dois - still 5169
-length(unique(res$data$id))
+# find unique ids
+length(unique(index2019$id)) # 4233
+length(unique(index2018$id)) # 4122
 
 
 # Don't  be  surprised  if  queries  you  perform  in  a  scripting  
-# language,  like  usingrplosin  R,  givedifferent results than when 
-# searching for articles on the PLOS website.  I am not sure what 
-# exactdefaults they use on their website.  There are a few things to 
-# consider.  You can tweak which typesof articles are returned: Try 
-# using the article_type filter in the fq parameter.  
-# For which journalto search, e.g., do'journal_key:PLoSONE'. 
-# Seejournalnamekey()for journal abbreviations
+# language,  like  using rplos in  R,  give different results than when 
+# searching for articles on the PLOS website. I am not sure what 
+# exact defaults they use on their website.
 
-# ISSN PlosOne = 1932-6203 
-# ISSN PsychScience = 1467-9280 (web, I find 166), 0956-7976 (print)
+# Sample 99 articles from 2019, 114 from 2018
+set.seed(17080904)
+sample2019 <- sample(index2019$id,99)
+sample2018 <- sample(index2018$id,114)
+sample2019 <- cbind(sample2019,rep(2019,length(sample2019)))
+sample2018 <- cbind(sample2018,rep(2018,length(sample2018)))
+plossample <- rbind(sample2018,sample2019)
 
-ps <- cr_journals(issn = c('1467-9280'), 
-            filter = c(from_pub_date = '2019-01-01', 
-                      until_pub_date = '2019-12-31'),  
-            works = T,
-            limit = 1000)
-
-plos <- cr_journals(query = "psychology",
-                    issn = c('1932-6203'), 
-                    filter = c(from_pub_date = '2019-01-01', 
-                               until_pub_date = '2019-12-31',
-                               has_full_text = TRUE),  
-                    works = T,
-                    limit = 1000)
-
-# we can't filter on subject so for Plos the number is way too large. 
-
-
-# Get 2 random dois
-cr_r(2)
+# Save to file to input in main codebook
+writeLines(plossample, "plossample.txt")
 
 
